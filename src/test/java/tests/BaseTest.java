2ffe29911370529c6e66acb49f447a864ff325f0
EldarAbdulhakov <braid2012@gmail.com>
2025-11-26 02:17:22 +0300

    task 7. Add grid.

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,28 +4,31 @@ import io.qameta.allure.Allure;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.io.ByteArrayInputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public abstract class BaseTest {
 
-    private WebDriver driver;
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     protected WebDriver getDriver() {
-        return driver;
+        return driver.get();
     }
 
     @BeforeMethod
-    public void setUp() {
+    public void setUp() throws MalformedURLException {
         ChromeOptions options = new ChromeOptions();
+        URL gridUrl = new URL("http://localhost:4444/wd/hub");
         options.addArguments("--window-size=1920,1080");
 
-        driver = new ChromeDriver(options);
+        driver.set(new RemoteWebDriver(gridUrl, options));
     }
 
     @AfterMethod
@@ -34,6 +37,7 @@ public abstract class BaseTest {
             byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
             Allure.addAttachment("ScreenshotWhenFalling", new ByteArrayInputStream(screenshot));
         }
-        driver.quit();
+        driver.get().quit();
+        driver.remove();
     }
 }
---
+++ src/test/resources/grid/start-grid.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Starting Selenium GRID HUB...
+start java -jar selenium-server-4.38.0.jar hub --host 127.0.0.1 --port 4444
+
+timeout /t 5
+
+echo Starting Selenium NODE (maxSessions=5)...
+start java -jar selenium-server-4.38.0.jar node --max-sessions 5 --detect-drivers true --hub http://127.0.0.1:4444
+echo GRID started:
+echo Hub URL: http://localhost:4444/ui
