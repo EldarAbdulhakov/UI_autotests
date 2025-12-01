65778e0986308138a3a1231519515d315d9ea270
EldarAbdulhakov <braid2012@gmail.com>
2025-12-01 23:12:11 +0300

    task 12

--- src/test/java/tests/AlertTest.java
+++ src/test/java/tests/AlertTest.java
@@ -1,16 +1,23 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import page.AlertPage;
 
 public class AlertTest extends BaseTest {
 
     @Test
-    public void test() throws InterruptedException {
-        new AlertPage(getDriver())
-                .clickInputAlert()
-                .clickDemonstrateInputBoxButton();
+    public void testEnterTextToAlert() {
+        final String alertFieldText = "Vincent van Gogh";
 
-        Thread.sleep(2000);
+        String inputBoxText = new AlertPage(getDriver())
+                .clickInputAlert()
+                .switchToInputBoxFrame()
+                .clickInputBoxButton()
+                .switchToAlert()
+                .enterTextToAlertAndAccept(alertFieldText)
+                .getInputBox();
+
+        Assert.assertEquals(inputBoxText, "Hello %s! How are you today?".formatted(alertFieldText));
     }
 }
